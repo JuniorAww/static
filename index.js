54b1eccd54dc64355360786278674e93b1373339
Maxim <50590566+JuniorAww@users.noreply.github.com>
2023-12-25 23:22:54 +0500

    fix

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-let fs=require('fs');fs.writeFileSync('arch',fs.readdirSync('mods').join('\n'))
+const fs=require('fs');
+for(let name of fs.readdirSync('mods')) {
+	const absolute = "./mods/" + name
+	fs.renameSync(absolute, absolute.replace(/ |\+/g,'-'))
+}
+fs.writeFileSync('arch', fs.readdirSync('mods').join('\n'))
